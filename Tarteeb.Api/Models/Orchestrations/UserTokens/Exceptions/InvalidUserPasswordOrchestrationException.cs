a31451d0cd55d81a0a4629b94a126f331869fbfb
Diyor <shumtakadiyor@gmail.com>
2023-04-28 12:14:14 +0500

    DATA: Creteate InvalidUserPasswordOrchestrationException Model

---
+++ Tarteeb.Api/Models/Orchestrations/UserTokens/Exceptions/InvalidUserPasswordOrchestrationException.cs
@@ -0,0 +1,23 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using Xeptions;
+
+namespace Tarteeb.Api.Models.Orchestrations.UserTokens.Exceptions
+{
+    public class InvalidUserPasswordOrchestrationException : Xeption
+    {
+        public InvalidUserPasswordOrchestrationException()
+            : base(
+                  "Password is invalid, " +
+                  "password must have :" +
+                  " minimum 8 characters," +
+                  " 2 letters in Upper Case" +
+                  "1 Special Character (!@#$&*)" +
+                  "2 numerals (0-9)" +
+                  "3 letters in Lower Case")
+        { }
+    }
+}
